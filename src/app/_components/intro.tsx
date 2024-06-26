a4bc5c80fb9232e2ee5e12dd01c63c2e6eb3580e
Johannes Schnepel <jojo599k@tutanota.de>
2024-06-26 16:49:54 +0200

    fixed some texts

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -7,7 +7,7 @@ export function Intro() {
         Fruchtfleisch-Studien.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Ein Block über Fruchtfleisch-Studien.
+        Ein Blog über Fruchtfleisch-Studien.
       </h4>
     </section>
   );
